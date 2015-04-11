b54454059a4c4633f070faef0daa621867a6ea13
Javier González Cabezas <javiergonzalezcabezas@mbp-de-javier.mynet>
2015-04-11 16:05:36 +0100

    add parse

--- platforms/android/assets/www/js/map.js
+++ platforms/android/assets/www/js/map.js
@@ -16,10 +16,35 @@
           center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
           mapTypeId: google.maps.MapTypeId.TERRAIN
         });
+        loadPoints();
     }
 
     // Show an alert if there is a problem getting the geolocation
     //
     function onError() {
         alert('onError!');
+    }
+    function loadPoints() {
+        var request = new XMLHttpRequest();
+        request.open("GET", "http://transportapi.com/v3/uk/tube/stations/near.json?lat=51.527789&lon=-0.102323&&page=1&rpp=10&&app_id=e7198ca5&api_key=08056cfd96db1080b221b4668e9e5734", true);
+        request.onreadystatechange = function() {//Call a function when the state changes.
+            if (request.readyState == 4) {
+                if (request.status == 200 || request.status == 0) {
+                    var points = JSON.parse(request.responseText);
+                    var show = document.getElementById("latestTweets");
+                                        show.innerHTML = points;
+                    for (i = 0; i < points.stations.length; i++) {
+                        points[i].longitude;
+                        points[i].latitude;
+                       alert( points[i].name);
+
+                    }
+
+
+
+                }
+            }
+        }
+        console.log("asking for tweets");
+        request.send();
     }
